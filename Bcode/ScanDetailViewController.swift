8cb4970c2e05937e9e065fa08f975457e4f09dab
jian sun <jiansun@jiandeMacBook-Pro.local>
2019-03-09 19:43:21 +0900

    ホームに戻る機能追加
    機能：ホームに戻る機能追加

--- Bcode/ScanDetailViewController.swift
+++ Bcode/ScanDetailViewController.swift
@@ -10,10 +10,24 @@ import UIKit
 
 class ScanDetailViewController: UIViewController {
 
+    // 简介
     @IBOutlet weak var label1: UILabel!
+    //response
     var text1:String!
+    //商品名
+    @IBOutlet weak var label2: UILabel!
     
     @IBOutlet weak var image1: UIImageView!
+    
+    
+    @IBAction func backButton(_ sender: UIButton) {
+        self.presentingViewController?.presentingViewController?.dismiss(animated: true, completion: nil)
+    }
+    
+    
+    
+    
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -21,13 +35,17 @@ class ScanDetailViewController: UIViewController {
         //response分析：data:日本商品名；中国商品名；画像URL
         let dataArray = text1.split(separator: ";")
 
-        let jpPro = dataArray[0]
+//        let jpPro = dataArray[0]
         let znPro = dataArray[1]
         let img   = String(dataArray[2])
         let detail = String(dataArray[3])
 
 
-        let product = "日本名："+jpPro+"\n"+"中文名："+znPro+"\n"+"\n"+"简介："+detail
+//        let product = "日本名："+jpPro+"\n"+"中文名："+znPro+"\n"+"\n"+"简介："+detail
+
+        let product_name = "中文名："+znPro
+        
+        let product_detail = "简介："+detail
 
         
         //画像表示
@@ -35,7 +53,10 @@ class ScanDetailViewController: UIViewController {
         image1.image = image
 
         //商品名表示
-        label1.text = String(product)
+        label2.text = String(product_name)
+
+        //简介表示
+        label1.text = String(product_detail)
 
     }
     
