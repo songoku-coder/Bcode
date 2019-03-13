35b86b37d1fd4fb8666c23b1c41eb75a2738ce66
jian sun <jiansun@jiandeMacBook-Pro.local>
2019-03-13 22:56:19 +0900

    button枠線追加

--- Bcode/ScanDetailViewController.swift
+++ Bcode/ScanDetailViewController.swift
@@ -16,6 +16,7 @@ class ScanDetailViewController: UIViewController {
     var text1:String!
     //商品名
     @IBOutlet weak var label2: UILabel!
+    @IBOutlet weak var priceLabel: UILabel!
     
     @IBOutlet weak var image1: UIImageView!
     
@@ -25,25 +26,25 @@ class ScanDetailViewController: UIViewController {
     }
     
     
+    @IBOutlet weak var likeButton: UIButton!
     
-    
-    
+    @IBOutlet weak var goWebButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        //response分析：data:日本商品名；中国商品名；画像URL
+        //response分析：data:日本商品名；中国商品名；画像URL；値段；レビュー
         let dataArray = text1.split(separator: ";")
 
 //        let jpPro = dataArray[0]
         let znPro = dataArray[1]
         let img   = String(dataArray[2])
         let detail = String(dataArray[3])
+        
+        let price = String(dataArray[4])
 
 
-//        let product = "日本名："+jpPro+"\n"+"中文名："+znPro+"\n"+"\n"+"简介："+detail
-
-        let product_name = "中文名："+znPro
+        let product_name = znPro
         
         let product_detail = "简介："+detail
 
@@ -54,9 +55,26 @@ class ScanDetailViewController: UIViewController {
 
         //商品名表示
         label2.text = String(product_name)
+        label2.sizeToFit()
+        
+        //値段
+        priceLabel.text = "官方指导价格: "+price + "日元"
+        priceLabel.sizeToFit()
 
         //简介表示
         label1.text = String(product_detail)
+        label1.sizeToFit()
+        
+        //button設定
+        likeButton.backgroundColor = UIColor.white // 背景色
+        likeButton.layer.borderWidth = 0.5 // 枠線の幅
+        likeButton.layer.borderColor = UIColor.blue.cgColor // 枠線の色
+        likeButton.layer.cornerRadius = 10.0 // 角丸のサイズ
+        
+        goWebButton.backgroundColor = UIColor.white // 背景色
+        goWebButton.layer.borderWidth = 0.5 // 枠線の幅
+        goWebButton.layer.borderColor = UIColor.blue.cgColor // 枠線の色
+        goWebButton.layer.cornerRadius = 10.0 // 角丸のサイズ
 
     }
     
